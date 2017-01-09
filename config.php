c656dfd51c559bee9a7b8e75660282cc20ba4fb2
remusmesar <remusmesar@gmail.com>
2017-01-09 03:33:44 -0500

    versiune initiala proiect

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://e-pliante.avantondigital.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://e-pliante.avantondigital.com/');
+
+// DIR
+define('DIR_APPLICATION', '/remus/www/epliante/public_html/catalog/');
+define('DIR_SYSTEM', '/remus/www/epliante/public_html/system/');
+define('DIR_IMAGE', '/remus/www/epliante/public_html/image/');
+define('DIR_LANGUAGE', '/remus/www/epliante/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/remus/www/epliante/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/remus/www/epliante/public_html/system/config/');
+define('DIR_CACHE', '/remus/www/epliante/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/remus/www/epliante/public_html/system/storage/download/');
+define('DIR_LOGS', '/remus/www/epliante/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/remus/www/epliante/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/remus/www/epliante/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'remus');
+define('DB_PASSWORD', 'webdev2016');
+define('DB_DATABASE', 'epliante');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
